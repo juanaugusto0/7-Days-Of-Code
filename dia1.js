5b3436a0906d17a134f62985098c3472c776c7d6
juanaugusto0 <114126909+juanaugusto0@users.noreply.github.com>
2022-10-14 10:32:45 -0300

    Primeiro Teste

---
+++ dia1.js
@@ -0,0 +1,14 @@
+let num1 = 1;
+let str1 = '1';
+let num30 = 30;
+let str30 = '30';
+let num10 = 10;
+let str10 = '10';
+
+
+if (num1!==str1) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+}else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
+
