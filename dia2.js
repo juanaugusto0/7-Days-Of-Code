f10aab4db2208b5815418f555c1605e048027533
juanaugusto0 <114126909+juanaugusto0@users.noreply.github.com>
2022-10-15 12:43:25 -0300

    Dia 2 Finalizado

--- dia2.js
+++ dia2.js
@@ -1,1 +1,13 @@
+const nome = prompt("Qual seu nome?  ");
+const idade = prompt("Quantos anos você tem?  ");
+const linguagem = prompt("Qual linguagem de programação você está estudando?  ");
 
+console.log (`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+const gostar = prompt (`Você gosta de estudar ${linguagem}? Digite 1 para sim e 2 para não.  `);
+
+if (gostar==1){
+    console.log ("Que pena, já tentou outra linguagem?")
+}else {
+    console.log ("Que bom, se esforce nos estudos!")
+}
