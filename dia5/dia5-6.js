4fccd23eb0e0d9cf19b380e68b59624d012563a8
juanaugusto0 <114126909+juanaugusto0@users.noreply.github.com>
2022-10-19 11:12:10 -0300

    Erro no dia 6, não consigo resolver

--- dia5/dia5-6.js
+++ dia5/dia5-6.js
@@ -2,6 +2,28 @@ let frutas = [];
 let laticinios = [];
 let doces = [];
 
+function removerItens () {
+	var negacao = confirm ("Você quer remover algum item?")
+	if (negacao) {
+		const remocao = prompt ("Diga qual item você quer remover")
+		let indexArray;
+		if (frutas.indexOf(remocao) != -1) {
+			indexArray = frutas.indexOf(remocao);
+			frutas.splice(indexArray,indexArray);
+		}
+		if (laticinios.indexOf(remocao) != -1) {
+			indexArray = laticinios.indexOf(remocao);
+			laticinios.splice(indexArray,indexArray);
+		}
+		if (doces.indexOf(remocao) != -1) {
+			indexArray = doces.indexOf(remocao);
+			doces.splice(indexArray,indexArray);
+		}else {
+			alert ("Valor não encontrado");
+		}
+	}
+}
+
 for (let index = 0; index < 999; index++) {
 	const addItens = prompt ("Que item você quer adicionar?");
 	const addCategorias = prompt ("Digite a categoria na qual você quer encaixar o item, 1 para frutas, 2 para laticinios e 3 para congelados");
@@ -14,6 +36,7 @@ for (let index = 0; index < 999; index++) {
 	if (addCategorias == 3){
 	    doces.push(addItens);
 	}
+	removerItens ();
     var confirmacao = confirm("Você quer adicionar mais itens?");
         if (!confirmacao){
             index = 999;
